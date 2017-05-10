01f265732b24cb170cd8bb2968fbe74ba4ec7403
DylanLaFrenz <dcl75@nau.edu>
2017-05-09 21:10:06 -0700

    hfjdkashf

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-// server.js
+/**
+ * Created by dylanlafrenz on 5/2/17.
+ */
 const express = require('express');
 const app = express();
+
 // Run the app by serving the static files
 // in the dist directory
+
 app.use(express.static(__dirname + '/dist'));
 
+// Start the app by listening on the default
+// Heroku port
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
